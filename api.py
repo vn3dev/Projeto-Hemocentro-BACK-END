28dd2333bce8654043486dc32c15ea62cb986590
Vinicius Costa <vinicosta.vn3dev@gmail.com>
2026-05-21 21:20:00 -0300

    fix: remove comentarios e visao geral

--- api.py
+++ api.py
@@ -1,18 +1,14 @@
 from flask import Flask
 from routes.doadores import doadores_bp
 from routes.bolsas import bolsas_bp
-from routes.visao_geral import visao_geral_bp
 from flask_cors import CORS
 
 app = Flask(__name__)
-CORS = CORS(app)  # Habilita CORS para toda a aplicação
+CORS = CORS(app)
 
-# mantem a ordem dos atributos na hora de exibir o json pq sou fresco
 app.json.sort_keys = False
 
-# registra os blueprints
 app.register_blueprint(doadores_bp)
 app.register_blueprint(bolsas_bp)
-app.register_blueprint(visao_geral_bp)
 
 app.run(debug = True)
